e6fd9251787a4ca8d3ef90143dfc6216cd76cfc4
goose <purification1997@gmail.com>
2025-02-06 21:42:44 +0700

    feat(actions): add Astro language support

--- lua/timber/actions/treesitter.lua
+++ lua/timber/actions/treesitter.lua
@@ -19,17 +19,39 @@ function M.sort_ts_nodes_preorder(nodes)
   end)
 end
 
+local function get_language_tree(lang)
+  local bufnr = vim.api.nvim_get_current_buf()
+  local parser = vim.treesitter.get_parser(bufnr, lang)
+
+  if lang == "astro" then
+    -- Astro has injected Typescript syntax
+    local child_parser = parser:children()["typescript"]
+    return child_parser:parse()[1]
+  else
+    return parser:parse()[1]
+  end
+end
+
+-- Some languages have injected languages, e.g. Astro has Typescript injected
+local function get_logable_lang(lang)
+  if lang == "astro" then
+    return "typescript"
+  else
+    return lang
+  end
+end
+
 ---@alias logable_range {[1]: number, [2]: number}
 ---@param lang string
 ---@param range {[1]: number, [2]: number, [3]: number, [4]: number}
 ---@return {container: TSNode, logable_ranges: logable_range[]}[]
 function M.query_log_target_containers(lang, range)
   local bufnr = vim.api.nvim_get_current_buf()
-  local parser = vim.treesitter.get_parser(bufnr, lang)
-  local tree = parser:parse()[1]
+  local tree = get_language_tree(lang)
   local root = tree:root()
 
-  local query = vim.treesitter.query.get(lang, "timber-log-container")
+  local logable_lang = get_logable_lang(lang)
+  local query = vim.treesitter.query.get(logable_lang, "timber-log-container")
   if not query then
     utils.notify(string.format("timber doesn't support %s language", lang), "error")
     return {}
@@ -63,7 +85,8 @@ end
 ---@param lang string
 ---@return {container: TSNode, log_targets: TSNode[]}[]
 function M.query_log_targets(containers, lang)
-  local query = vim.treesitter.query.get(lang, "timber-log-target")
+  local logable_lang = get_logable_lang(lang)
+  local query = vim.treesitter.query.get(logable_lang, "timber-log-target")
   if not query then
     utils.notify(string.format("timber doesn't support %s language", lang), "error")
     return {}
---
+++ tests/timber/actions/lang/astro_spec.lua
@@ -0,0 +1,135 @@
+local timber = require("timber")
+local helper = require("tests.timber.helper")
+local actions = require("timber.actions")
+
+describe("astro single log", function()
+  before_each(function()
+    timber.setup({
+      log_templates = {
+        default = {
+          astro = [[console.log("%log_target", %log_target)]],
+        },
+      },
+    })
+  end)
+
+  it("supports Typescript code in frontmatter", function()
+    local input = [[
+      ---
+      const fo|o = "foo"
+      const bar = "bar"
+      ---
+      <!doctype html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>%title</title>
+        </head>
+        <body>
+          <p>foo: {foo}</p>
+          <p>bar: {bar}</p>
+        </body>
+      </html>
+    ]]
+
+    local expected = [[
+      ---
+      const foo = "foo"
+      console.log("foo", foo)
+      const bar = "bar"
+      ---
+      <!doctype html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>%title</title>
+        </head>
+        <body>
+          <p>foo: {foo}</p>
+          <p>bar: {bar}</p>
+        </body>
+      </html>
+    ]]
+
+    helper.assert_scenario({
+      input = input,
+      filetype = "astro",
+      action = function()
+        -- For some reason, we have to call treesitter.start() to properly initialize the parser
+        vim.treesitter.start(0, "astro")
+        helper.wait(20)
+        actions.insert_log({ position = "below" })
+      end,
+      expected = expected,
+    })
+  end)
+end)
+
+describe("astro batch log", function()
+  before_each(function()
+    timber.setup({
+      log_templates = {
+        default = {
+          astro = [[console.log("%log_target", %log_target)]],
+        },
+      },
+    })
+  end)
+
+  it("supports Typescript code in frontmatter", function()
+    local input = [[
+      ---
+      const fo|o = "foo"
+      const bar = "bar"
+      ---
+      <!doctype html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>%title</title>
+        </head>
+        <body>
+          <p>foo: {foo}</p>
+          <p>bar: {bar}</p>
+        </body>
+      </html>
+    ]]
+
+    local expected = [[
+      ---
+      const foo = "foo"
+      const bar = "bar"
+      console.log({ "foo": foo, "bar": bar })
+      ---
+      <!doctype html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>%title</title>
+        </head>
+        <body>
+          <p>foo: {foo}</p>
+          <p>bar: {bar}</p>
+        </body>
+      </html>
+    ]]
+
+    helper.assert_scenario({
+      input = input,
+      filetype = "astro",
+      action = function()
+        -- For some reason, we have to call treesitter.start() to properly initialize the parser
+        vim.treesitter.start(0, "astro")
+        helper.wait(20)
+        vim.cmd("normal! Vj")
+        actions.add_log_targets_to_batch()
+        actions.insert_batch_log()
+      end,
+      expected = expected,
+    })
+  end)
+end)
